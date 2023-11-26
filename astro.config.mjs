20f20888de7bf08f6865d05ab6fbbb55b484fb54
Caleb <me@calebdelbridge.com>
2023-11-26 13:04:36 -0500

    animations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://calebdelbridge.com",
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), react()],
   output: "hybrid",
-  adapter: vercel({ webAnalytics: { enabled: true }, imageService: true }),
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
+    },
+    imageService: true
+  })
 });
